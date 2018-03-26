0115e550aff198461697ab86512557c737615b9e
Hendrik Dalagan <naskapie@gmx.net>
2018-03-26 21:40:45 +0200

    Update Problem_007.java

--- Java/Problem_007.java
+++ Java/Problem_007.java
@@ -3,30 +3,40 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
  */
 
-public class Problem_007 {
+/*
+By listing the first six prime numbers: 
+2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+*/
 
-	public static void main(String[] args) {
-		System.out.println(isCount(10001));
-	}
-	
-	public static int isCount(int count) {
-		int num = 0;
-		int j = 0;
-		for (int i=2; i<=9999999; i++) {
-			if (isPrime(i))
-				j++; 
-			if (j==count)
-				return i;
+public class Problem_007 {
+	public static void main(String [] args) {
+		long start = System.currentTimeMillis(); 
+		
+		int i = 1;
+		int count = 0; 
+		while(count<10001) {
+			i++;
+			if(isPrim(i)) {
+				count++;
+			}
 		}
-		return -1;
+		long stop = System.currentTimeMillis(); 
+		System.out.println(i);
+		System.out.println(stop - start + " ms");
 	}
 	
-	public static boolean isPrime(double num){
-		if(num < 2)
-			return false;
-		for(int i = 2; i <= Math.sqrt(num); i++)
-			if(num % i == 0)
-				return false;
-		return true;
+	public static boolean isPrim(int n) {
+		boolean b = true;
+		int j = 2; 
+		while(j<=Math.sqrt(n)) {
+				if(n%j == 0) {
+					b = false; 
+					break; 
+				}
+				j++; 
+		}
+		return b; 
 	}
 }
+
