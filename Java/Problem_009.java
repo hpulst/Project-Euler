6c3584cf6e4033cc6bb8f8eabcb7356b080e5a3f
Hendrik Dalagan <naskapie@gmx.net>
2018-03-26 21:44:36 +0200

    Update Problem_009.java

--- Java/Problem_009.java
+++ Java/Problem_009.java
@@ -10,21 +10,19 @@ Find the product abc.
 import java.util.*;
 
 public class Problem_009{
-	
 	public static void main(String[] args) {
 		int sum = 1000; 
 		for(int i=1; i<sum; i++) {
 			for(int j=1; j<sum; j++) {
 				for(int k=1; k<sum; k++) {
 					if(isTriplet(i,j,k,sum))
-						System.out.println(i + "\n" +  j + "\n"+ k + "\n" + i*j*k);
-						
+						System.out.println("Answer: " + i*j*k);
+
 				}
 			}
 		}
-		System.out.println("fail");
 	}
-	
+
 	public static boolean isTriplet(int a, int b, int c, int sum) {
 		if(a*a+b*b==c*c && a+b+c==sum && a<b && b<c)
 			return true;
