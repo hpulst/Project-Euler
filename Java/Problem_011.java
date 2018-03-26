eaf4ef9238328f55257ee8c1e24fe02d24f028d8
Hendrik Dalagan <naskapie@gmx.net>
2018-03-26 21:46:59 +0200

    Update Problem_011.java

--- Java/Problem_011.java
+++ Java/Problem_011.java
@@ -1,66 +1,69 @@
 /*
 In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-import java.io.*;
-import java.util.Scanner; 
-
-public class Problem_011 {
-
-	public static void main(String[] args) throws FileNotFoundException {
-		String Filepath = "numbers.txt"; 
-		Scanner scan = new Scanner(new File(Filepath));
+public class Problem_011{
+	public static void main(String [] args) throws FileNotFoundException {
+		Scanner s = new Scanner(new File("Problem_011.txt"));
+		long start = System.currentTimeMillis(); 
+		int t = 0, m = 0; 
 		int[][] Grid = new int[20][20]; 
-		int product = 0; 
-		int largest = 0; 
-
-		for (int i=0; i<20; i++) {
-			for (int j=0; j<20; j++) {
-				if(scan.hasNextInt()) {
-					Grid[i][j] = scan.nextInt(); 
+		
+		for(int i=0; i<20; i++) {
+			for(int j=0; j<20; j++) {
+				if(s.hasNextInt()) {
+					Grid[i][j] = s.nextInt();
 				}
 			}
 		}
+		s.close();
 
-		// check horizontally
-		for (int i=0; i<20; i++) {
+//left, right
+		for(int i=0; i<20; i++) {
 			for(int j=0; j<17; j++) {
-				System.out.println(Grid[i][j]);
-				product = Grid[i][j] * Grid[i][j+1]* Grid[i][j+2]*Grid[i][j+3]; 
-				if (product > largest)
-					largest = product; 
-			}
+				t = Grid[i][j] * Grid[i][j+1] * Grid[i][j+2] * Grid[i][j+3]; 
+				if(t>m) {
+					m = t; 
+				}
+			}	
+		}
+		
+//up,down
+		for(int i=0; i<17; i++) {
+			for(int j=0; j<20; j++) {
+				t = Grid[i][j] * Grid[i+1][j] * Grid[i+2][j] * Grid[i+3][j]; 
+				if(t>m) {
+					m = t; 
+				}
+			}	
 		}
 
-		// Check vertically
-		for(int i = 0; i < 17; i ++){
-			for(int j = 0; j < 20; j++){
-				product = Grid[i][j] * Grid[i + 1][j] * Grid[i + 2][j] * Grid[i + 3][j];
-				if(product > largest){
-					largest = product;
+
+//right diagonally
+		for(int i=0; i<17; i++) {
+			for(int j=0; j<17; j++) {
+				t = Grid[i][j] * Grid[i+1][j+1] * Grid[i+2][j+2] * Grid[i+3][j+3]; 
+				if(t>m) {
+					m = t; 
 				}
-			}
+			}	
+		}				
+				
+//left diagonally 
+		for(int i=0; i<17; i++) {
+			for(int j=3; j<20; j++) {
+				t = Grid[i][j] * Grid[i+1][j-1] * Grid[i+2][j-2]* Grid[i+3][j-3]; 
+				if(t>m) {
+					m = t; 
+				}
+			}	
 		}
 
-		// Check diagonally right
-		for(int i = 0; i < 17; i++){
-			for(int j = 0; j < 17; j++){
-				product = Grid[i][j] * Grid[i + 1][j + 1] * Grid[i + 2][j + 2] * Grid[i + 3][i + 3];
-				if(product > largest){
-					largest = product;
-				}
-			}
-		}
-
-		// Check diagonally left
-		for(int i = 0; i < 17; i ++){
-			for(int j = 3; j < 20; j ++){
-				product = Grid[i][j] * Grid[i + 1][j - 1] * Grid[i + 2][j  - 2] * Grid[i + 3][j - 3];
-				if(product > largest){
-					largest = product;
-				}
-			}
-		}
-		System.out.println(largest);		
+		long stop = System.currentTimeMillis(); 
+		System.out.println("sum: " + m );
+		System.out.println(stop - start + " ms");
 	}
 }
