2892c2d77047626f4487756f94f20a420da81009
Hendrik Dalagan <naskapie@gmx.net>
2018-03-26 21:55:52 +0200

    Update Problem_014.java

--- Java/Problem_014.java
+++ Java/Problem_014.java
@@ -1,32 +1,36 @@
-import java.io.*;
-import java.util.Scanner; 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigInteger;
+import java.util.Scanner;
 
-public class Problem_014 {
-
-	public static void main(String[] args) throws FileNotFoundException {
-		long max = 1; 
-		long i;
-		long length = 0; 
-		long maxIdx = 0; 
-		for (i=1; i<=1000000; i++) {
-			length = makeSeq(i); 
-			if(length > max) {
-				max = length;
-				maxIdx = i; 
+public class Problem_014{
+	public static void main(String [] args) throws FileNotFoundException {
+		long start = System.currentTimeMillis(); 
+		
+		long j = 0, i = 0, m = 1, n = 1; 
+		while(i<1000000){
+			i++;
+			j = collatz(i);
+			if(j>m) {
+				m = j;
+				n = i;
 			}
 		}
-		System.out.println("Max: " + maxIdx + "  CollatzSeq: " + max);
+
+		long stop = System.currentTimeMillis(); 
+		System.out.println("sum: " + m + " num: " + n);
+		System.out.println(stop - start + " ms");
 	}
 	
-	public static long makeSeq(long c) { 
-		long chainSize = 1;
-		while(c != 1) {
-			if(c % 2 ==0)
-				c = c/2;
+	public static int collatz (long i) {  
+		int j = 1; 
+		while(i!=1 && i!=0) {
+			if (i%2==0)
+				i = i/2; 
 			else 
-				c = c*3 +1;
-			chainSize++; 
+				i = 3*i + 1; 
+			j++;
 		}
-		return chainSize; 
+		return j; 
 	}
 }
