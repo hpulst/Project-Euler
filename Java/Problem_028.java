3e7d10b069738b945075743fe328359becd92a7d
Hendrik Dalagan <naskapie@gmx.net>
2018-03-26 21:58:36 +0200

    Update Problem_028.java

--- Java/Problem_028.java
+++ Java/Problem_028.java
@@ -3,11 +3,16 @@ What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed 
  */
 
 public class Problem_028 {
-   public static void main(String[] args) {
+	public static void main(String [] args) {
+		long start = System.currentTimeMillis(); 
+		
 		int n = 1001, p = 1; 
 		for (int i=3; i<=n; i+=2) {
 			p += 4*(i*i)-6*i + 6; 
 		}
+		
+		long stop = System.currentTimeMillis(); 
 		System.out.println("Answer: " + p);
+		System.out.println(stop - start + " ms");
 	}
 }
