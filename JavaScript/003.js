91fd10945a30f0cc8917df3e115e80697ad9eca8
emanuel <naskapie@gmx.net>
2019-03-03 15:45:57 +0100

    Task 4

--- JavaScript/003.js
+++ JavaScript/003.js
@@ -1,10 +1,24 @@
 "use strict";
 
-// Largest prime factor
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475
 
-let number = 600851475143;
 
-function largestPrimeFactorOf(
+const number = 600851475143;
+
+function largestPrimeFactorOf(number){
+    var factor = 2;
+
+    while(factor <= number){
+        if(number % factor == 0){
+            number /= factor;
+        } else {
+            factor++;
+        }
+    }
+    return factor;
+
 }
+
 
 module.exports = largestPrimeFactorOf(number);
