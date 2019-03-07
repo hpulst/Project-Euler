e31fe58a59d2188b6b6e78bbec0ba6028c7d060d
emanuel <naskapie@gmx.net>
2019-03-07 17:08:26 +0100

    Added 7

--- JavaScript/005.js
+++ JavaScript/005.js
@@ -1,1 +1,24 @@
-console.log("Hi");
+// 2520 is the smallest number that can be divided by each of the numbers
+// from 1 to 10 without any remainder.
+
+// What is the smallest number that is evenly divisible by all of the numbers
+// from 1 to 20?
+
+function noRemainder(top) {
+  var num = top;
+  while (true) {
+    var find = true;
+    for (var i = top; i > 1; i--) {
+      if (num % i !== 0) {
+        find = false;
+        break;
+      }
+    }
+    if (find === true) {
+      return num;
+    }
+    num += top;
+  }
+}
+
+module.exports = noRemainder(20);
