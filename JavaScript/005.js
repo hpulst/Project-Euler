30da424fcc206b340d45bae0341506ce0c49921e
emanuel <naskapie@gmx.net>
2019-04-11 12:52:15 +0200

    5

--- JavaScript/005.js
+++ JavaScript/005.js
@@ -1,27 +1,59 @@
 'use strict'; 
 
-// 2520 is the smallest number that can be divided by each of the numbers
-// from 1 to 10 without any remainder.
+// Time 00:11:13.341 all-time record
 
-// What is the smallest number that is evenly divisible by all of the numbers
-// from 1 to 20?
+// function noRemainder(n) {
+//   let find = false,
+//     m = n;
+//   while (find == false) {
+//     for (let i = 1; i <= n; i++) {
+//       if (m % i !== 0) {
+//         find == false;
+//         break;
+//       }
+//       if (i === n) {
+//         find = true;
+//         return m;   
+//       }
+//     }
+//     m++;
+//   }
+// }
 
-function noRemainder(n) {
-  let find = false,
-    m = n;
-  while (find == false) {
-    for (let i = 1; i <= n; i++) {
-      if (m % i !== 0) {
-        find == false;
-        break;
+// module.exports = noRemainder(20);
+
+
+function smallestDivisible(limit) {
+  var i, n = 1;
+
+  function largestPower(n, limit) {
+      var p, e = 2, largest = n;
+      while ((p = Math.pow(n, e)) <= limit) {
+          largest = p;
+          e += 1;
       }
-      if (i === n) {
-        find = true;
-        return m;   
-      }
-    }
-    m++;
+      return largest;
   }
+
+  function isPrime(n) {
+      var i, limit = Math.ceil(Math.sqrt(n));
+      // since the main loop generates odd numbers only
+      // we can start testing primality dividing by 3
+      for (i = 3; i <= limit; i += 2) {
+          if (n % i === 0) {
+              return false;
+          }
+      }
+      return true;
+  }
+
+  for (i = 3; i <= limit; i += 2) {
+      if (isPrime(i)) {
+          n *= largestPower(i, limit);
+      }
+  }
+
+  return n * largestPower(2, limit);
 }
 
-module.exports = noRemainder(20);
+module.exports = smallestDivisible(20);
--- JavaScript/013.js
+++ JavaScript/013.js
@@ -2,31 +2,3 @@
 
 // Time 01:18:44.363
 
-let limit = 2000000; 
-let  p = 2;
-let primeNumbers = [];
-let sum = 0;
-
-
-function calsHero(){
-    while(p < Math.floor(Math.sqrt(limit))){
-        for(let i = 2; i < limit; i+= p){
-            primeNumbers[i] = false;
-        }
-
-        for(let j = p; j < limit; j++){
-            if(primeNumbers[j] === undefined){
-                p = j; 
-                break;
-            }
-        }
-    }
-
-    for(k = 2; k < limit; k++){
-        if(primeNumbers[k] === undefined){
-            sum+= k;
-        }
-    }
-}
-
-module.exports = calsHero();
