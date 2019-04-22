65e4d408308bd270e2f52a8b6e3f920258b516ed
emanuel <naskapie@gmx.net>
2019-04-22 23:48:33 +0200

    Improve 12

--- JavaScript/012.js
+++ JavaScript/012.js
@@ -2,17 +2,44 @@
 
 //Time 01:01:37.098
 
+/* It is based on coprime numbers properties. The triangle number is equal to 
+n*(n+1)/2 where n and n+1 are coprimes => n and (n+1)/2 
+are coprimes or n/2 and n+1 are coprimes ( it depends if n is even or odd ).  */
 
-let sum = 1, t = 1, count = 0; 
-while (count<=500) {
-  count = 0;
-  for (let j = 1; j<=Math.sqrt(sum); j++) {
-    if(sum % j === 0) {
-      count += 2;
+
+function getTriangle(n) {
+    const THRESHOLD_DIVISORS = 500;
+    let numberOfDivisors = 0;
+    let firstCoprime, secondCoprime;
+    while (true) {
+        if (n % 2 == 0) {
+            firstCoprime = getNumberOfDivisors(n / 2);
+            secondCoprime = getNumberOfDivisors(n + 1);
+            
+        } else {
+            firstCoprime = getNumberOfDivisors(n);
+            secondCoprime = getNumberOfDivisors((n + 1) / 2);
+        }
+        numberOfDivisors = firstCoprime * secondCoprime;
+        if (numberOfDivisors > THRESHOLD_DIVISORS) {
+            return n * (n + 1) / 2;
+        }
+        n++;
     }
-  }
-  t++;
-  sum += t;
 }
 
-module.exports = sum - t;
+function getNumberOfDivisors(nr) {
+    let divisors = 0;
+    let sqrt = Math.sqrt(nr);
+    for (let i = 1; i <= sqrt; i++) {
+        if (nr % i == 0) {
+            divisors += 2;           // E.g.: (2, n/2), (3, n/3)
+        }
+    }
+    if (sqrt * sqrt == nr) {        // it was counted twice
+        divisors--;
+    }
+    return divisors;
+}
+
+module.exports = getTriangle(1);
